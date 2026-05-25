4abee09f178abe209162ddabff68cafc516a43a7
Muhammad Luthfi <muhammadluthfi250201@gmail.com>
2026-05-26 00:52:51 +0700

    feat: Menambahkan aset brand Platinum Gym

---
+++ resources/views/partials/head-brand.blade.php
@@ -0,0 +1,4 @@
+<title>{{ $title ?? config('app.name', 'Platinum Gym Padang') }}</title>
+<link rel="icon" href="{{ asset('favicon.ico') }}" sizes="any">
+<link rel="icon" href="{{ asset('favicon.svg') }}" type="image/svg+xml">
+<link rel="apple-touch-icon" href="{{ asset('apple-touch-icon.png') }}">
