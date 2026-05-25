cbcaffa0259faba3f0850ca439f7c668138bf63c
Muhammad Luthfi <muhammadluthfi250201@gmail.com>
2026-05-26 00:53:01 +0700

    feat: Membuat layout auth Platinum Gym

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,8 @@ import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
 export default {
+    darkMode: 'class',
+
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
@@ -13,6 +15,13 @@ export default {
         extend: {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                gold: {
+                    400: '#FFD978',
+                    500: '#FEAC18',
+                    600: '#D9961F',
+                },
             },
         },
     },
