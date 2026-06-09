fc105df92759982d9c28a43eeeb8e4d6f7bbd121
Muhammad Luthfi <muhammadluthfi250201@gmail.com>
2026-06-09 09:47:26 +0700

    feat: Menambahkan data media produk dan galeri

--- database/seeders/GymClassSeeder.php
+++ database/seeders/GymClassSeeder.php
@@ -12,10 +12,10 @@ class GymClassSeeder extends Seeder
     public function run(): void
     {
         $classes = [
-            ['Aerobic', 'senam', 'included', 'senam', 25, 0, 45000, 21000, [['Coach Ola', 1], ['Coach Irgo', 3], ['Coach Ola', 5]], '17:00', '18:00'],
-            ['Zumba', 'senam', 'included', 'senam', 25, 0, 45000, 21000, [['Zin Nila', 2], ['Zin Nila', 4]], '17:00', '18:00'],
+            ['Aerobic', 'senam', 'included', 'senam', 25, 0, 45000, null, [['Coach Ola', 1], ['Coach Irgo', 3], ['Coach Ola', 5]], '17:00', '18:00'],
+            ['Zumba', 'senam', 'included', 'senam', 25, 0, 45000, null, [['Zin Nila', 2], ['Zin Nila', 4]], '17:00', '18:00'],
             ['Poundfit', 'poundfit', 'paid', null, 25, 50000, 50000, null, [['Coach Ajeng', 3]], '19:15', '20:15'],
-            ['Muaythai', 'muaythai', 'session_based', 'muaythai', 6, null, null, null, [['Coach Arie', 1], ['Coach Adi', 1], ['Coach Arie', 2], ['Coach Adi', 2], ['Coach Arie', 3], ['Coach Adi', 3], ['Coach Arie', 4], ['Coach Adi', 4], ['Coach Arie', 5], ['Coach Adi', 5], ['Coach Arie', 6], ['Coach Adi', 6]], '19:00', '20:00'],
+            ['Muaythai', 'muaythai', 'session_based', 'muaythai', 6, null, 85000, null, [['Coach Arie', 1], ['Coach Adi', 1], ['Coach Arie', 2], ['Coach Adi', 2], ['Coach Arie', 3], ['Coach Adi', 3], ['Coach Arie', 4], ['Coach Adi', 4], ['Coach Arie', 5], ['Coach Adi', 5], ['Coach Arie', 6], ['Coach Adi', 6]], '19:00', '20:00'],
         ];
 
         foreach ($classes as [$name, $classType, $accessType, $requiredPackageType, $capacity, $memberPrice, $nonMemberPrice, $promoPrice, $schedules, $startTime, $endTime]) {
